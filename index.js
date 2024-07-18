c9d463e02ba3ddc0fa89c12de10299586d75712e
Ariba S <153156108+asiddiqi16@users.noreply.github.com>
2024-07-18 15:46:15 +1000

    Added JS elements for injecting poem body

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem-body", {
+    strings: "This is a poem about love",
+    delay: 100,
+    autoStart: true,
+    cursor: "",
+  });
+}
 
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
